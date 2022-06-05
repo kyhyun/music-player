63bff039a094429d744ce04582afe157533898f9
kyhyun <k29176@gmail.com>
2022-06-05 15:43:56 +0900

    Chore : Update environment config and remove unused files

---
+++ src/assets/music/index.ts
@@ -0,0 +1,32 @@
+import track1 from './spiralMoon.mp3'
+import track2 from './Polaris-Memories.mp3'
+import track3 from './TheMorningStarAndTheBoySong.mp3'
+import track4 from './Reflection.mp3'
+import track5 from './Wizard-Dinner.mp3'
+import track6 from './Kaya-Toast.mp3'
+import track7 from './when-spring-comes.mp3'
+import track8 from './MEGALOVANIA.mp3'
+
+interface Isong {
+  track1: string
+  track2: string
+  track3: string
+  track4: string
+  track5: string
+  track6: string
+  track7: string
+  track8: string
+}
+
+const song: Isong = {
+  track1,
+  track2,
+  track3,
+  track4,
+  track5,
+  track6,
+  track7,
+  track8,
+}
+
+export default song
